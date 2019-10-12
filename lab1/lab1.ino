a6d3cc781a3c6127d2f94e4f2825f21c7e4a4fe6
Yewert <blazer.andrew@gmail.com>
2019-10-12 11:25:59 +0500

    added some logic and models

--- lab1/lab1.ino
+++ lab1/lab1.ino
@@ -1,8 +1,17 @@
 #include "button.h"
+#include "rgb.h"
 
 #define R_OUT 6
 #define G_OUT 7
 #define B_OUT 8
+
+RGB ledState = RGB();
+Button redInc = Button(...);
+Button greenInc = Button(...);
+Button blueInc = Button(...);
+Button redDec = Button(...);
+Button greenDec = Button(...);
+Button blueDec = Button(...);
 
 
 void setup()
@@ -14,12 +23,56 @@ void setup()
 
 void loop() 
 {
-    set_rgb_led(10, 10, 10);
+    set_rgb_led(ledState);
+    if (redInc.wasPressed())
+    {
+    	ledState.red = inc_number_with_max(ledState.red, 16);
+    	return;
+    }
+    if (redDec.wasPressed())
+    {
+    	ledState.red = dec_number_with_min(ledState.red, 16);
+    	return;
+    }
+    if (greenInc.wasPressed())
+    {
+    	ledState.green = inc_number_with_max(ledState.green, 16);
+    	return;
+    }
+	if (greenDec.wasPressed())
+    {
+    	ledState.green = dec_number_with_min(ledState.green, 16);
+    	return;
+    }
+    if (blueInc.wasPressed())
+    {
+    	ledState.blue = inc_number_with_max(ledState.blue, 16);
+    	return;
+    }
+	if (blueDec.wasPressed())
+    {
+    	ledState.green = dec_number_with_min(ledState.blue, 16);
+    	return;
+    }
 }
 
-void set_rgb_led(int red, int green, int blue)
+void set_rgb_led(RGB rgb)
 {
-    analogWrite(R_OUT, 255 - red);
-    analogWrite(G_OUT, 255 - green);
-    analogWrite(B_OUT, 255 - blue);
+    analogWrite(R_OUT, 255 - rgb.red);
+    analogWrite(G_OUT, 255 - rgb.green);
+    analogWrite(B_OUT, 255 - rgb.blue);
+}
+
+int inc_number_with_max(int val, int inc)
+{
+	int sum = val + inc;
+
+	return sum > 255 ? 255 : sum;
+}
+
+int dec_number_with_min(int val, int dec)
+{
+	int diff = val - dec;
+
+	return diff < 0 ? 0 : diff;
 }
---
+++ lab1/rgb.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class RGB
+{
+	public:
+		RGB()
+		{
+			red = 0;
+			green = 0;
+			blue = 0;
+		}
+
+		int red;
+		int green;
+		int blue;
+}
